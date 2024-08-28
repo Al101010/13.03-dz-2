0e04442dbd133c0d0d487b0eccf6452aed613d71
alexgithub <alexgithub@yandex.ru>
2024-08-28 12:28:38 +1000

    commit-17

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 13.03-dz-2
 27.08.2024 доделываю ДЗ "Обработка событий"
+
+Бэйджик AppVeyor:
+[![Сборка status](https://ci.appveyor.com/api/projects/status/0sgb65oj2rhx2fll?svg=true)](https://ci.appveyor.com/project/Al101010/13-03-dz-2)
+
+Ссылка:
+https://al101010.github.io/13.03-dz-2/
--- src/js/view/title.js
+++ src/js/view/title.js
@@ -8,6 +8,26 @@ function createDiv(textTitle) {
 
 createDiv('Попаданий: <span>0</span><br>Промахов: <span>0</span><br>');
 
+function createH3_2(textTitle) {
+    const titleH3 = document.createElement('h3');
+    titleH3.classList = 'titleH3';
+    titleH3.textContent = textTitle;
+
+    document.querySelector('body').prepend(titleH3);
+}
+
+createH3_2('- считается промахом');
+
+function createH3_1(textTitle) {
+    const titleH3 = document.createElement('h3');
+    titleH3.classList = 'titleH3';
+    titleH3.textContent = textTitle;
+
+    document.querySelector('body').prepend(titleH3);
+}
+
+createH3_1('Пропустил 5 появлений гоблина(или 5 секунд)');
+
 
 function createH1(textTitle) {
     const titleH1 = document.createElement('h1');
